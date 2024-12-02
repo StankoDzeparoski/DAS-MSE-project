ec04fc3f06561f946280464b5496454c029abc95
Stanko Djeparoski <stankodzepar@gmail.com>
2024-12-02 23:27:31 +0100

    mockups -> guimockup assets folder for pages

--- Homework2/HomeWork2Java/src/main/java/mk/ukim/finki/wp/homework2das/web/controller/IssuerWebController.java
+++ Homework2/HomeWork2Java/src/main/java/mk/ukim/finki/wp/homework2das/web/controller/IssuerWebController.java
@@ -24,9 +24,17 @@ public class IssuerWebController {
         this.tickerService = tickerService;
     }
 
+    // Mapping to home page
+    @GetMapping("/")
+    public String showHomePage(Model model) {
+//        List<Ticker> tickers = tickerService.findAll();
+//        model.addAttribute("tickers", tickers);
+        return "home"; // Name of the Thymeleaf template (issuers.html)
+    }
+
     // Mapping to show all issuers in a Thymeleaf template
     @GetMapping("/issuers")
-    public String showAllIssuers(Model model) {
+    public String showAllIssuersPage(Model model) {
         List<Ticker> tickers = tickerService.findAll();
         model.addAttribute("tickers", tickers);
         return "issuers_list"; // Name of the Thymeleaf template (issuers.html)
@@ -34,7 +42,7 @@ public class IssuerWebController {
 
     // Mapping to show a single issuer by tickerCode and date
     @GetMapping("/issuers/{tickerCode}/{date}")
-    public String showIssuerDetails(@PathVariable String tickerCode, @PathVariable String date, Model model) {
+    public String showIssuerDetailsPage(@PathVariable String tickerCode, @PathVariable String date, Model model) {
         List<Issuer> issuers = issuerService.getIssuersByTickerCode(tickerCode);
         model.addAttribute("issuers", issuers);
         return "issuer-details"; // Another Thymeleaf template for details
